fd9bf4553f9887060fdc89cf2d0013b48a998aec
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2024-09-23 14:08:06 +0200

    chore: removed 'download theme' footer link

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -75,10 +75,6 @@ const siteConfig: SiteConfig = {
         {
             text: 'Terms',
             href: '/terms'
-        },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
         }
     ],
     socialLinks: [
