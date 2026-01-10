47fa20b21c1c00ba30f2e13947ca663011b506d0
David Rios <mr.daverios@gmail.com>
2026-01-10 17:40:45 -0500

    fix: external link icon color

--- src/icons/OpenExternalLink.tsx
+++ src/icons/OpenExternalLink.tsx
@@ -1,19 +1,15 @@
 export const OpenExternalLink: React.FC<React.SVGProps<SVGSVGElement>> = ({ className = '', ...props }) => (
     <svg
         viewBox="0 0 24 24"
-        stroke-width="1.5"
-        fill="none"
+        strokeWidth="1.5"
+        strokeLinecap="round"
         xmlns="http://www.w3.org/2000/svg"
-        color="#000000"
         className={className}
+        fill="none"
+        stroke="currentColor"
         {...props}
     >
-        <path d="M21 3L15 3M21 3L12 12M21 3V9" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-        <path
-            d="M21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H11"
-            stroke="#000000"
-            stroke-width="1.5"
-            stroke-linecap="round"
-        />
+        <path d="M21 3L15 3M21 3L12 12M21 3V9" strokeLinejoin="round" />
+        <path d="M21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H11" strokeLinecap="round" />
     </svg>
 );
